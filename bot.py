f29b40f53b693ece088e172639230366a821dcc8
SkyBlue <helloiamskyblue@gmail.com>
2020-02-12 12:28:03 +0800

    speed optimization & change help message

--- bot.py
+++ bot.py
@@ -19,7 +19,7 @@ bot = commands.Bot(command_prefix='~')
 async def on_ready():
     print('connected to Discord!')
 
-@bot.command(name='search')
+@bot.command(name='search', help='\'~search <something>\' | \'~search help\' to see help')
 async def search(ctx,name,category='orna'):
     if name == 'index':
         message = '資料庫已有目錄: ```'
@@ -27,18 +27,18 @@ async def search(ctx,name,category='orna'):
         message =message + indexString + '```'
         await ctx.send(message)
         return
-    titles=wks.find(name,cols=(1,1),matchEntireCell=True)
-    if not titles:
+    matchTitleRow = [ title.row for title in wks.get_col(1, returnas='cell', include_tailing_empty=False)[1::] if title.value == name]
+    if not matchTitleRow:
         try:
             await ctx.send('尚無資料，歡迎至 https://tinyurl.com/wxa9qxy 新增資料')
         except:
              pass
         return
-    for title in titles:
-        for item in wks.get_row(title.row,include_tailing_empty=False)[1::]:
-            try:
-                await ctx.send(item)
-            except:
-                pass
+    for row in matchTitleRow:
+        message = '\n---\n'.join(wks.get_row(row,include_tailing_empty=False)[1::])
+        try:
+            await ctx.send(message)
+        except:
+            pass
 
 bot.run(token)
