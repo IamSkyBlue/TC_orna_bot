8c3f93317947f869cc3458d7a0cbfcf2941f0f57
skyblue <bob890823@gmail.com>
2021-05-06 19:35:58 +0800

    revert

--- orna.py
+++ orna.py
@@ -22,18 +22,17 @@ class Orna(commands.Cog):
         self.bot = bot
 
     @commands.command(
-        name="search", help="使用方法: ~search <要搜尋的東西> |搜尋規則或幫助: ~search help| ~search index 可察看目錄"
+        name="search",
+        help="使用方法: ~search <要搜尋的東西> |搜尋規則或幫助: ~search help| ~search index 可察看目錄",
     )
     async def search(self, ctx, name):
-        if name == "index" or "目錄":
-            result = sorted(wks.get_col(1, include_tailing_empty=False)[1::]
-            await ctx.send("資料庫已有目錄: ")
-            while result:
-                indexString = "```"
-                while len(indexString) < 1900 and result:
-                    indexString = indexString + ',' + result.pop(0)
-                await ctx.send(indexString + "```")
-            return
+        if name == "index":
+            message = "資料庫已有目錄: ```"
+            indexString = " , ".join(
+                sorted(wks.get_col(1, include_tailing_empty=False)[1::])
+            )
+            message = message + indexString + "```"
+            await ctx.send(message)
 
         matchTitleRow = [
             title.row
@@ -44,7 +43,10 @@ class Orna(commands.Cog):
         ]
         if not matchTitleRow:
             try:
-                await ctx.send("尚無資料，歡迎至 https://tinyurl.com/wxa9qxy 新增資料")
+                await ctx.send("尚無資料，歡迎至 <https://tinyurl.com/wxa9qxy> 新增資料")
+                await ctx.send(
+                    "或是可以到中文圖書館自己尋找有用的資訊 <https://hackmd.io/@Iamskyblue/Orna_TW_index>"
+                )
             except:
                 pass
             return
