4b7eac4978f71b6c225988092d7c695d714f96d3
skyblue <bob890823@gmail.com>
2021-06-07 21:59:11 +0800

    featrue: img text detection to search string for ornabot

--- orna.py
+++ orna.py
@@ -1,29 +1,43 @@
 # encoding: utf-8
 import os
 import re
+import time
 
 import discord
 from discord.ext import commands, tasks
 from dotenv import load_dotenv
 
 import pygsheets
-from bs4 import BeautifulSoup
-import requests
 
+from google.cloud import vision
 
 load_dotenv()
-gc = pygsheets.authorize(service_account_env_var="GDRIVE_API_CREDENTIALS")
+gc = pygsheets.authorize(service_account_env_var="GOOGLE_APPLICATION_CREDENTIALS")
 Ornabook = gc.open("Ornabook")
 mainwks = Ornabook[0]
 wordwks = Ornabook[1]
 
 OrnaTCDB = gc.open("OrnaTCDB")
 TCDBmainwks = OrnaTCDB[0]
+imgporcesschannels = OrnaTCDB[1]
+
+visionclient = vision.ImageAnnotatorClient()
+
+IMAGE_TYPE = ("jpeg", "png", "webp")
+MATCH_WORD_TC = ("血量:", "魔力:", "護盾:", "物攻:", "物防:", "敏捷:", "魔攻:", "魔防:")
+MATCH_WORD_EN = ("HP:", "Mana:", "Ward:", "Att:", "Def:", "Dex:", "Mag:", "Res:")
 
 
 class Orna(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.updatetime = time.time()
+        self.imgporcesschannellist = imgporcesschannels.get_all_values(
+            returnas="matrix",
+            majdim="ROWS",
+            include_tailing_empty=False,
+            include_tailing_empty_rows=False,
+        )[1::]
 
     @commands.command(
         name="search",
@@ -109,6 +123,134 @@ class Orna(commands.Cog):
             (matchTitleRow[0], resultcol)
         ).value  # some string has duplicate translate
         await ctx.send(message)
+
+    @commands.command(
+        name="subscribe",
+        help="使用方法: ~subscribe | 使中文機器人監聽此頻道的圖片並嘗試辨識",
+    )
+    async def subscribe(self, ctx, *args):
+        channelmatrix = imgporcesschannels.get_all_values(
+            returnas="cell",
+            majdim="ROWS",
+            include_tailing_empty=False,
+            include_tailing_empty_rows=False,
+        )[1::]
+        otherregistered = False
+        for pair in channelmatrix:
+            if (
+                str(ctx.guild.id) == pair[0].value
+                and str(ctx.channel.id) == pair[1].value
+            ):
+                await ctx.send("本頻道已註冊過，無須再進行註冊")
+                return
+            elif (
+                str(ctx.guild.id) == pair[0].value
+                and str(ctx.channel.id) != pair[1].value
+            ):
+                otherregistered = True
+        imgporcesschannels.insert_rows(
+            1, values=[str(ctx.guild.id), str(ctx.channel.id)]
+        )
+        if otherregistered:
+            await ctx.send("成功註冊本頻道，偵測到其他頻道也有註冊\n若其他頻道轉為其他用途，記得使用~unsubscribe退出監聽頻道")
+        else:
+            await ctx.send("成功註冊本頻道，請注意10秒後機器人才會開始監聽本頻道")
+
+    @commands.command(
+        name="unsubscribe",
+        help="使用方法: ~unsubscribe | 移除中文機器人對此頻道的監聽",
+    )
+    async def unsubscribe(self, ctx, *args):
+        channelmatrix = imgporcesschannels.get_all_values(
+            returnas="cell",
+            majdim="ROWS",
+            include_tailing_empty=False,
+            include_tailing_empty_rows=False,
+        )
+        for pair in channelmatrix:
+            if (
+                str(ctx.guild.id) == pair[0].value
+                and str(ctx.channel.id) == pair[1].value
+            ):
+                imgporcesschannels.delete_rows(pair[0].row)
+                await ctx.send("已移除本頻道，可使用~subscribe再次訂閱")
+                return
+
+    async def process_img(self, ctx):
+        issubscribe = False
+        for pair in self.imgporcesschannellist:
+            if pair[0] == str(ctx.guild.id):
+                if pair[1] == str(ctx.channel.id):
+                    issubscribe = True
+        if not issubscribe:
+            return
+        for att in ctx.attachments:
+            attname = att.content_type.split("/")[1]
+            if attname not in IMAGE_TYPE:
+                return
+
+            image = vision.Image()
+            image.source.image_uri = att.url
+            response = visionclient.text_detection(image=image)
+            texts = response.text_annotations
+            if not texts:
+                return  # exceed limit or something
+
+            textlist = texts[0].description.split("\n")
+            untrans_itemnamestr = ""
+            levelstr = ""
+            statstr = ""
+            for textindex in range(0, len(textlist)):
+                if textlist[textindex] == "儲藏室":
+                    untrans_itemnamestr = textlist[textindex - 1]
+                elif textlist[textindex].startswith("等級"):
+                    levelstr = textlist[textindex]
+                    levelstr = levelstr.replace(" ", "")
+                    levelstr = levelstr.replace("等級", "")
+                    levelstr = " (" + levelstr + ") "
+                elif any(keyword in textlist[textindex] for keyword in MATCH_WORD_TC):
+                    statstr += textlist[textindex]
+            if not untrans_itemnamestr:
+                return  # can't find chinese keyword in the image, quit process
+            for TCstr, ENstr in zip(MATCH_WORD_TC, MATCH_WORD_EN):
+                statstr = statstr.replace(TCstr, ENstr)
+            statstr = statstr.replace(",", "")
+            if not levelstr:
+                levelstr = " (1) "
+
+            allmatchTitleRow = []
+            data = TCDBmainwks.get_col(3, returnas="cell", include_tailing_empty=False)[
+                2::
+            ]
+            strindex = len(untrans_itemnamestr) - 1
+            while strindex >= 0:  # loop search DB using [-1::],[-2::].....[0::]
+                matchTitleRow = [
+                    title.row
+                    for title in data
+                    if title.value.lower() == untrans_itemnamestr[strindex::]
+                ]
+                allmatchTitleRow.extend(matchTitleRow)
+                strindex -= 1
+            itemnamestrindex = allmatchTitleRow[-1]  # max len match string
+            itemnamestr = TCDBmainwks.cell((itemnamestrindex, 2)).value
+
+            searchstr = "`%assess " + itemnamestr + levelstr + statstr + "`"
+            searchstr = searchstr.replace("\n", " ")
+            searchstr = searchstr.replace("  ", " ")
+            await ctx.channel.send(searchstr)
+
+    @commands.Cog.listener()
+    async def on_message(self, ctx):
+        if ctx.attachments:
+            if time.time() - self.updatetime > 10:
+                self.imgporcesschannellist = imgporcesschannels.get_all_values(
+                    returnas="matrix",
+                    majdim="ROWS",
+                    include_tailing_empty=False,
+                    include_tailing_empty_rows=False,
+                )[1::]
+                self.updatetime = time.time()
+            await self.process_img(ctx)
 
 
 def setup(bot):
